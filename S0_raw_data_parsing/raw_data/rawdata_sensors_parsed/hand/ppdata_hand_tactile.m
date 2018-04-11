cb455874c2ed658fd0b41f42433c97a9de125ba1
alpais <lucia.pais@epfl.ch>
2018-04-11 13:41:46 +0200

    adding parsed hand data

---
+++ S0_raw_data_parsing/raw_data/rawdata_sensors_parsed/hand/ppdata_hand_tactile.m
@@ -0,0 +1,57 @@
+
+
+
+subj_start  = 14;
+subj_end    = 14;
+
+% load('excepted_demos');
+excepted_demos{52}.hand_all = [];
+
+for snum = subj_start:subj_end
+    
+    fprintf('       >> Processing Subject %d \n', snum);
+    data_dir = sprintf('../../rawdata_sensors_bags/S%.2d/', snum);
+    data_files = dir(strcat(data_dir, '*.txt'));
+    
+    
+%     excepted_demos{snum}.hand_all = zeros(1, length(excepted_demos{snum}.Tool_FT));
+    
+    for dem = 1:length(data_files)
+        data = load(strcat(data_dir, data_files(dem).name));
+        
+        if isempty(data)
+            excepted_demos{snum}.hand_all(dem) = 1;
+            fprintf(2, 'Demo %.2d no finger or tactile data \n', dem);
+            continue
+        end
+        
+        if (sum(data(300, 763:785) - data(10, 763:785))==0)%(sum(sum(data(:, 763:785)~=0)) == 0)
+            excepted_demos{snum}.hand_all(dem) = 1;
+            fprintf(2, 'Demo %.2d no finger JA data \n', dem);
+            continue
+        end
+        
+        if (sum(sum(data(:, 727:744)~=0)) == 0)
+            excepted_demos{snum}.hand_all(dem) = 1;
+            fprintf(2, 'Demo %.2d no tactile data \n', dem);
+            continue
+        end
+        
+        rawdata_hand{dem}.finger_ja       = data(:, 763:785);
+        rawdata_hand{dem}.tactile_front   = data(:, 727:744);
+        rawdata_hand{dem}.tactile_side    = data(:, 745:762);
+        
+        clear data
+    end
+    
+    if exist('rawdata_hand', 'var')
+        sfoutdir = sprintf('../../rawdata_sensors_parsed/hand');
+        sfoutname = sprintf('%s/S%.2d_rawdata_hand.mat', sfoutdir, snum);
+        save(sfoutname, 'rawdata_hand', '-mat');
+    end
+    
+    clear data_dir data_files rawdata_hand
+    
+end
+
+save('excepted_demos.mat', 'excepted_demos', '-mat')
